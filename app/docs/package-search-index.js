4a265b522e9af6c87653d59bc468e72907968235
Kato Shinya <kato.shinya.dev@gmail.com>
2021-03-21 19:14:41 +0900

    add

---
+++ app/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"org.thinkit.api.gateway.github"},{"l":"org.thinkit.api.gateway.github.catalog"},{"l":"org.thinkit.api.gateway.github.response"},{"l":"org.thinkit.api.gateway.github.user"},{"l":"org.thinkit.api.gateway.github.util"},{"l":"すべてのパッケージ","u":"allpackages-index.html"}];updateSearchResults();
