35d991e6ee84b7634be0bd9342b21ae6af27a5e6
Kato Shinya <kato.shinya.dev@gmail.com>
2021-03-30 17:27:41 +0900

    update

--- app/src/main/java/org/thinkit/api/gateway/github/util/CommunicationResolver.java
+++ app/src/main/java/org/thinkit/api/gateway/github/util/CommunicationResolver.java
@@ -27,6 +27,16 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.gson.GsonFactory;
 
+import org.thinkit.api.catalog.BiCatalog;
+import org.thinkit.api.gateway.github.catalog.ErrorHttpStatus;
+import org.thinkit.api.gateway.github.exception.AccessForbiddenException;
+import org.thinkit.api.gateway.github.exception.BadGatewayException;
+import org.thinkit.api.gateway.github.exception.BadRequestException;
+import org.thinkit.api.gateway.github.exception.InternalServerErrorException;
+import org.thinkit.api.gateway.github.exception.NotAcceptableException;
+import org.thinkit.api.gateway.github.exception.NotFoundException;
+import org.thinkit.api.gateway.github.exception.ServiceUnavailableException;
+import org.thinkit.api.gateway.github.exception.UserUnauthorizedException;
 import org.thinkit.api.gateway.github.user.OAuthConfig;
 
 import lombok.AccessLevel;
@@ -124,7 +134,7 @@ public final class CommunicationResolver implements Serializable {
     private HttpResponse sendGetRequest(@NonNull final GenericUrl genericUrl) throws IOException {
         final HttpRequest httpRequest = HTTP_REQUEST_FACTORY.buildGetRequest(genericUrl);
         httpRequest.getHeaders().setAuthorization(SecuritySchemeResolver.bearer(this.oAuthConfig.getAccessToken()));
-        return httpRequest.setParser(JSON_OBJECT_PARSER).execute();
+        return this.checkHttpStatus(httpRequest.setParser(JSON_OBJECT_PARSER).execute());
     }
 
     /**
@@ -159,5 +169,52 @@ public final class CommunicationResolver implements Serializable {
     private <T> List<T> parseAsList(@NonNull final HttpResponse httpResponse,
             @NonNull final ParameterizedType parameterizedType) throws IOException {
         return (List<T>) httpResponse.parseAs(parameterizedType);
+    }
+
+    /**
+     * Check the status code of the HTTP response.
+     *
+     * <p>
+     * The exception will always be raised at runtime if a status code indicating a
+     * client error and a server error is detected. If no error is detected, the
+     * HTTP response passed as an argument will be returned as is.
+     *
+     * @param httpResponse The http response
+     * @return The http response passed as an argument
+     *
+     * @exception NullPointerException If {@code null} is passed as an argument
+     */
+    private HttpResponse checkHttpStatus(@NonNull final HttpResponse httpResponse) {
+
+        final ErrorHttpStatus errorHttpStatus = BiCatalog.getEnumByTag(ErrorHttpStatus.class,
+                httpResponse.getStatusCode());
+
+        if (errorHttpStatus == null) {
+            return httpResponse;
+        }
+
+        switch (errorHttpStatus) {
+        case BAD_REQUEST -> throw new BadRequestException(this.getStatusMessage(httpResponse));
+        case UNAUTHORIZED -> throw new UserUnauthorizedException(this.getStatusMessage(httpResponse));
+        case FORBIDDEN -> throw new AccessForbiddenException(this.getStatusMessage(httpResponse));
+        case NOT_FOUND -> throw new NotFoundException(this.getStatusMessage(httpResponse));
+        case NOT_ACCEPTABLE -> throw new NotAcceptableException(this.getStatusMessage(httpResponse));
+        case INTERNAL_SERVER_ERROR -> throw new InternalServerErrorException(this.getStatusMessage(httpResponse));
+        case BAD_GATEWAY -> throw new BadGatewayException(this.getStatusMessage(httpResponse));
+        case SERVICE_UNAVAILABLE -> throw new ServiceUnavailableException(this.getStatusMessage(httpResponse));
+        default -> throw new IllegalStateException(); // it will never happen
+        }
+    }
+
+    /**
+     * Returns the http status message from HTTP response.
+     *
+     * @param httpResponse The HTTP response
+     * @return The HTTP status message
+     *
+     * @exception NullPointerException If {@code null} is passed as an argument
+     */
+    private String getStatusMessage(@NonNull final HttpResponse httpResponse) {
+        return httpResponse.getStatusMessage();
     }
 }
