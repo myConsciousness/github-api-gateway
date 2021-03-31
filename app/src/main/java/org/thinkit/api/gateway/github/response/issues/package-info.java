f6333a9e54bf07ab7425bb925044c84a50287695
Kato Shinya <kato.shinya.dev@gmail.com>
2021-03-31 16:47:38 +0900

    update

---
+++ app/src/main/java/org/thinkit/api/gateway/github/response/issues/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The package that manages the response type for the GitHub API
+ * {@code "https://api.github.com/repos/repositoryDomain/issues"} .
+ */
+package org.thinkit.api.gateway.github.response.issues;
